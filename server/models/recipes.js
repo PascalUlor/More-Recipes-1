609c6bd5da2f2e2012996f0b22edd097493e60b4
chykehyman <hyginuschinwoke@gmail.com>
2017-09-29 04:03:21 +0100

    Delete 'author' attribute and replace with 'userId'

--- server/models/recipes.js
+++ server/models/recipes.js
@@ -1,24 +1,24 @@
 const recipes = [{
         id: 1,
-        author: 'Chinwoke Hyginus',
         title: 'Nigerian Ewedu Soup',
         ingredients: [
             'maggie', 'pepper', 'onions'
         ],
         directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
         upvotes: 6,
-        downvotes: 2
+        downvotes: 2,
+        userId: 2
     },
     {
         id: 2,
-        author: 'Frank Benjamin',
         title: 'American Crunchy Burger',
         ingredients: [
             'maggie', 'pepper', 'onions'
         ],
         directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
         upvotes: 6,
-        downvotes: 2
+        downvotes: 2,
+        userId: 1
     }
 ];
 
