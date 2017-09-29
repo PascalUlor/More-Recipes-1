26307baf524a4c7406129542b7f0c9dfbb500861
chykehyman <hyginuschinwoke@gmail.com>
2017-09-29 03:31:26 +0100

    Modify model to reflect an array of objects structure

--- server/models/recipes.js
+++ server/models/recipes.js
@@ -1,25 +1,25 @@
-export default {
-    recipes: [{
-            id: 1,
-            author: 'Chinwoke Hyginus',
-            title: 'Nigerian Ewedu Soup',
-            ingredients: [
-                'maggie', 'pepper', 'onions'
-            ],
-            directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
-            upvotes: 6,
-            downvotes: 2
-        },
-        {
-            id: 2,
-            author: 'Frank Benjamin',
-            title: 'American Crunchy Burger',
-            ingredients: [
-                'maggie', 'pepper', 'onions'
-            ],
-            directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
-            upvotes: 6,
-            downvotes: 2
-        }
-    ]
-};
+const recipes = [{
+        id: 1,
+        author: 'Chinwoke Hyginus',
+        title: 'Nigerian Ewedu Soup',
+        ingredients: [
+            'maggie', 'pepper', 'onions'
+        ],
+        directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
+        upvotes: 6,
+        downvotes: 2
+    },
+    {
+        id: 2,
+        author: 'Frank Benjamin',
+        title: 'American Crunchy Burger',
+        ingredients: [
+            'maggie', 'pepper', 'onions'
+        ],
+        directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
+        upvotes: 6,
+        downvotes: 2
+    }
+];
+
+export default recipes;
