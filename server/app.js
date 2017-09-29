00768136f2deb1f5eb1ae5cc2d6ef3a8fe4d5086
chykehyman <hyginuschinwoke@gmail.com>
2017-09-29 07:51:18 +0100

    Modify GET route for default  welcome message from '/api' to '/'

--- server/app.js
+++ server/app.js
@@ -8,13 +8,12 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200);
     res.json({
         name: 'Chike',
         message: 'Welcome to More-Recipes'
     });
 });
-
 
 app.listen(port, () => console.log(`Application started on port ${port}`));
