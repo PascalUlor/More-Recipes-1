118cc2a60feeddee4fbff545da3ace22c5a4f9c7
chykehyman <hyginuschinwoke@gmail.com>
2017-10-01 16:12:33 +0100

    Change attribute 'directions' to 'procedures'

--- server/models/recipes.js
+++ server/models/recipes.js
@@ -2,7 +2,7 @@ const recipes = [{
         id: 1,
         title: 'Nigerian Ewedu Soup',
         ingredients: 'maggie, pepper, onions',
-        directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
+        procedures: 'Boil water for about 20 minutes. Sleeve your onions properly.',
         upvotes: 10,
         downvotes: 2,
         userId: 2
@@ -11,7 +11,7 @@ const recipes = [{
         id: 2,
         title: 'American Crunchy Burger',
         ingredients: 'vegetable, bread, beef',
-        directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
+        procedures: 'Boil water for about 20 minutes. Sleeve your onions properly.',
         upvotes: 14,
         downvotes: 4,
         userId: 1
@@ -20,7 +20,7 @@ const recipes = [{
         id: 3,
         title: 'Indian Tomato Sauce',
         ingredients: 'fresh tomatoes, green pepper, onions',
-        directions: 'Boil water for about 20 minutes. Sleeve your onions properly.',
+        procedures: 'Boil water for about 20 minutes. Sleeve your onions properly.',
         upvotes: 20,
         downvotes: 0,
         userId: 1
