55d9767b89f081f7a0b15f1714825091b4278f10
chykehyman <hyginuschinwoke@gmail.com>
2017-10-02 04:36:04 +0100

    Implement test case for loading application home page

--- server/specs/tests.js
+++ server/specs/tests.js
@@ -7,3 +7,21 @@
 
  const { expect } = chai,
  request = supertest(app);
+
+ describe('All test cases for application', () => {
+     describe('Test case for loading application home page', () => {
+         it('should load application home page', (done) => {
+             request.get('/')
+                 .set('Content-Type', 'application/json')
+                 .expect(200)
+                 .end((err, res) => {
+                     expect(res.body).deep.equal({
+                         name: 'Chike',
+                         message: 'Welcome to More-Recipes'
+                     });
+                     if (err) done(err);
+                     done();
+                 });
+         });
+     });
+ });
