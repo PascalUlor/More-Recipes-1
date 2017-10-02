24717f6d39d219fe4d7c35294843e0c971d590af
chykehyman <hyginuschinwoke@gmail.com>
2017-10-02 14:56:34 +0100

    Modify response message for 'deleteRecipe' from 'Delete was successful' to 'Successfully deleted recipe'

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -94,7 +94,7 @@ export default class RecipesApiController {
             res.status(200);
             res.json({
                 status: 'Success',
-                message: 'Delete was successful',
+                message: 'Successfully deleted recipe',
                 newRecipeCatalog
             });
         } else {
