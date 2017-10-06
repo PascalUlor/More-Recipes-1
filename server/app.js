09e9ede44ea9d238cb565965cfa3780e870ba9ee
chykehyman <hyginuschinwoke@gmail.com>
2017-10-06 08:08:43 +0100

    Modify app.js

--- server/app.js
+++ server/app.js
@@ -9,30 +9,16 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'access-control-allow-methods,access-control-allow-origin,x-access-token,content-type,Origin, X-Requested-With, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 app.get('/', (req, res) => {
     res.status(200);
     res.json({
-        name: 'Chike',
-        message: 'Welcome to More-Recipes'
+        project: 'More-Recipes',
+        message: 'Share your awesome recipes ideas'
     });
 });
 
 app.use('/api/v1/', apiRoutes);
-app.use('/api/v1/', (req, res) => {
-    res.status(404);
-    res.json({
-        status: 'Failed',
-        message: 'Page not found'
-    });
-});
 
 app.use('*', (req, res) => {
     res.status(404);
