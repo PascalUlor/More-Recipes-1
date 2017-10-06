c4ed21960786143d16b2b3add44c5ba75e2fd9a8
chykehyman <hyginuschinwoke@gmail.com>
2017-10-06 07:14:59 +0100

    Implement POST request reviews validation

---
+++ server/middleware/validations/reviews.js
@@ -0,0 +1,42 @@
+import validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
+
+
+/**
+ * Validates all requests for reviews route
+ * @class ReviewsValidation
+ */
+export default class ReviewsValidation {
+    /**
+     * Validates all reviews details before allowing access to controller class
+     * @param {object} req
+     * @param {object} res
+     * @param {object} next
+     * @returns {object} Validation error messages or content of req.body passed to controller
+     */
+    static addReviewsValidation(req, res, next) {
+        const { reviewBody } = req.body,
+            errors = {};
+        // check for undefined inputs
+        if (reviewBody === undefined) {
+            res.status(400)
+                .json({
+                    status: 'Failed',
+                    message: 'Review body field is not defined'
+                });
+        }
+        // validation for review body
+        if (validator.isEmpty(reviewBody)) {
+            errors.reviewBody = 'Review body is required';
+        }
+
+        const result = { isValid: isEmpty(errors) };
+
+        if (!result.isValid) {
+            res.status(400)
+                .json({ errors });
+        } else {
+            next();
+        }
+    }
+}
