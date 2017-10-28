41e05a2a3c43c2a5d574baff8287169e68d40118
chykehyman <hyginuschinwoke@gmail.com>
2017-10-28 11:53:09 +0100

    Refactor codebase for user sigup and signin validations

--- server/middleware/validations/users.js
+++ server/middleware/validations/users.js
@@ -9,10 +9,10 @@ import isEmpty from 'lodash/isEmpty';
 export default class Validation {
     /**
      * Validates all User signup details before allowing access to controller class
-     * @param {obj} req
-     * @param {obj} res
-     * @param {obj} next
-     * @returns {obj} Validation error messages or Validation success
+     * @param {object} req
+     * @param {object} res
+     * @param {object} next
+     * @returns {object} Validation error messages or contents of req.body
      */
     static signup(req, res, next) {
         const {
@@ -24,87 +24,89 @@ export default class Validation {
         } = req.body,
             errors = {};
         if (fullName === undefined || username === undefined || email === undefined || password === undefined || repassword === undefined) {
-            res.status(400);
-            res.json({
-                status: 'Failed',
-                message: 'All or some fields are not defined'
-            });
-        } else {
-            if (!(validator.isEmpty(fullName))) {
-                if (validator.toInt(fullName)) {
-                    errors.fullName = 'Full name should not start with number(s)';
+            res.status(400)
+                .json({
+                    status: 'Failed',
+                    message: 'All or some fields are not defined'
+                });
+        }
+
+        if (!(validator.isEmpty(fullName))) {
+            for (let character = 0; character < fullName.length; character += 1) {
+                if (validator.toInt(fullName[character])) {
+                    errors.fullName = 'Full name must not contain numbers';
+                    break;
                 }
-            } else { errors.fullName = 'Full name is required'; }
-
-            if (!(validator.isEmpty(username))) {
-                if (validator.toInt(username)) {
-                    errors.username = 'Username should not start with number(s)';
-                }
-            } else { errors.username = 'Username is required'; }
-
-            if (!(validator.isEmpty(email))) {
-                if (!(validator.isEmail(email))) {
-                    errors.email = 'Email is invalid';
-                }
-            } else { errors.email = 'Email is required'; }
-
-            if (!(validator.isEmpty(password))) {
-                if (validator.isLength(password, { min: 8, max: 30 })) {
-                    if (!(validator.isEmpty(repassword))) {
-                        if (!(validator.equals(validator.trim(repassword), validator.trim(password)))) {
-                            errors.password = 'Password and confirm password fields mismatched';
-                        }
-                    } else { errors.password = 'Password reconfirmation is required'; }
-                } else { errors.password = 'Password length must be between 8 and 30'; }
-            } else { errors.password = 'Password is required'; }
-
-            const result = { isValid: isEmpty(errors) };
-
-            if (!result.isValid) {
-                res.status(400);
-                res.json({ errors });
-            } else {
-                next();
             }
+        } else { errors.fullName = 'Full name is required'; }
+
+        if (!(validator.isEmpty(username))) {
+            if (validator.toInt(username)) {
+                errors.username = 'Username must not start with number(s)';
+            }
+        } else { errors.username = 'Username is required'; }
+
+        if (!(validator.isEmpty(email))) {
+            if (!(validator.isEmail(email))) {
+                errors.email = 'Email is invalid';
+            }
+        } else { errors.email = 'Email is required'; }
+
+        if (!(validator.isEmpty(password))) {
+            if (!validator.isLength(password, { min: 8, max: 30 })) {
+                errors.password = 'Password length must be between 8 and 30';
+            }
+        } else { errors.password = 'Password is required'; }
+
+        if (!(validator.isEmpty(repassword))) {
+            if (!(validator.equals(validator.trim(repassword), validator.trim(password)))) {
+                errors.repassword = 'Password mismatched';
+            }
+        } else { errors.repassword = 'Password confirmation is required'; }
+
+        const result = { isValid: isEmpty(errors) };
+
+        if (!result.isValid) {
+            res.status(400)
+                .json({ errors });
+        } else {
+            next();
         }
     }
 
     /**
-     * Validates signin form input fields before allowing access to the database
-     * @param {obj} req
-     * @param {obj} res
-     * @param {obj} next
-     * @returns {json} Validation error messages or Validation success
+     * Validates signin form input fields before allowing access to controller class
+     * @param {object} req
+     * @param {object} res
+     * @param {object} next
+     * @returns {object} Validation error messages or contents of req.body
      */
     static signin(req, res, next) {
-        const {
-            username,
-            password
-        } = req.body,
+        const { username, password } = req.body,
             errors = {};
         if (username === undefined || password === undefined) {
-            res.status(400);
-            res.json({
-                status: 'Failed',
-                message: 'Username or(and) password field(s) is(are) not defined'
-            });
+            res.status(400)
+                .json({
+                    status: 'Failed',
+                    message: 'Username or(and) password field(s) is(are) not defined'
+                });
+        }
+
+        if (validator.isEmpty(username)) {
+            errors.username = 'Username is required';
+        }
+
+        if (validator.isEmpty(password)) {
+            errors.password = 'Password is required';
+        }
+
+        const result = { isValid: isEmpty(errors) };
+
+        if (!result.isValid) {
+            res.status(400)
+                .json({ errors });
         } else {
-            if (validator.isEmpty(username)) {
-                errors.username = 'Username is required';
-            }
-
-            if (validator.isEmpty(password)) {
-                errors.password = 'Password is required';
-            }
-
-            const result = { isValid: isEmpty(errors) };
-
-            if (!result.isValid) {
-                res.status(400);
-                res.json({ errors });
-            } else {
-                next();
-            }
+            next();
         }
     }
 }
