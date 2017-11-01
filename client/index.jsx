934793a2ec651b861b55ee473ba9895361e0a3d5
chykehyman <hyginuschinwoke@gmail.com>
2017-11-01 15:57:56 +0100

    Modify store to include index reducer component and redux devTools functionaliy

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router, Route, browserHistory, Switch } from 'react-router-dom';
 import HomePage from './components/homePage.jsx';
 import SignupPage from './components/signupPage.jsx';
 import SigninPage from './components/signinPage.jsx';
+import indexReducer from './reducers/index';
 import '../template/public/scss/main.scss';
 
 const store = createStore(
-	(state = {}) => state,
-	applyMiddleware(thunk)
+	indexReducer,
+	compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f)
 );
 
 
