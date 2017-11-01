619527a978070468862ebae224618cf89f4a665b
chykehyman <hyginuschinwoke@gmail.com>
2017-11-02 00:23:48 +0100

    Implement user signup flash message

---
+++ client/components/flash/flashMessagesList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import FlashMessage from './flashMessage.jsx';
+
+class FlashMessagesList extends React.Component {
+  render() {
+    const messages = this.props.messages.map(message =>
+        <FlashMessage key={message.id} message={message} />);
+    return (
+      <div className='container text-center'>{messages}</div>
+    );
+  }
+}
+
+FlashMessagesList.propTypes = {
+  messages: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    messages: state.flashMessages
+  };
+}
+
+export default connect(mapStateToProps)(FlashMessagesList);
