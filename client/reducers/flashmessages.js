eada25223cefcda9f2edcda19b29f7c83cdcc13a
chykehyman <hyginuschinwoke@gmail.com>
2017-11-14 17:13:09 +0100

    Modify array object

--- client/reducers/flashmessages.js
+++ client/reducers/flashmessages.js
@@ -4,14 +4,11 @@ import { ADD_FLASH_MESSAGE } from '../actions/actionTypes/actionTypes';
 export default (state = [], action = {}) => {
     switch (action.type) {
         case ADD_FLASH_MESSAGE:
-            return [
-                ...state,
-                {
-                    id: shortid.generate(),
-                    type: action.message.type,
-                    text: action.message.text
-                }
-            ];
+            return [{
+                id: shortid.generate(),
+                type: action.message.type,
+                text: action.message.text
+            }];
 
         default:
             return state;
