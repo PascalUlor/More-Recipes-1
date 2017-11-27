6451b7f8f2c1ac916c22d40d554a60ef727779c1
chykehyman <hyginuschinwoke@gmail.com>
2017-11-27 21:18:14 +0100

    Refactor(protect-client-side-routes): modify file to dispatch empty flash message object when authenticated user log out
    - Import 'requireAuthentication' HOC
    - Modify token decoding logic
    - Add HOC to dashboard component's route
    - Add 'exact' Route property to signup, signin and dashboard component routes

--- client/actions/actionCreators/logOutActions.js
+++ client/actions/actionCreators/logOutActions.js
@@ -1,10 +1,12 @@
 import { setAuthorizationToken } from '../../utils/setAuthorizationToken';
 import { setCurrentUser } from './signinActions';
+import { addFlashMessage } from './flashmessages';
 
 export function logOutRequest() {
     return (dispatch) => {
         localStorage.removeItem('jwtToken');
         setAuthorizationToken(false);
         dispatch(setCurrentUser({}));
+        dispatch(addFlashMessage({}));
     };
 }
